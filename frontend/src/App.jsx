c83b15db1f6e96518c4fff75f0119ee080be65e6
geniussandip2008-lab <geniussandip2008@gmail.com>
2026-04-11 17:49:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import CreateMessage from './pages/CreateMessage'
+import MessageHistory from './pages/MessageHistory'
+import RecipientMessage from './pages/RecipientMessage'
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create-message" element={<CreateMessage />} />
+        <Route path="/messages" element={<MessageHistory />} />
+        <Route path="/m/:link" element={<RecipientMessage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
